c1b086ead690dc2ccc5bea6b6a765c20425a76c1
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-16 14:07:44 -0400

    feat: create Tweet ui

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,13 +2,71 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 // API helpers
 import { formatTweet } from "../utils/helpers";
+import { formatDate } from "../utils/helpers";
+// Icons
+import { BsFillReplyFill, BsHeartFill, BsHeart } from "react-icons/bs";
 
 export class Tweet extends Component {
+  toParent = (e, id) => {
+    e.preventDefault();
+    // TODO: redirect to parent tweet
+  };
+
+  handleHeart = (e) => {
+    e.preventDefault();
+    // TODO: handle a like event
+  };
+
   render() {
-    return !this.props.tweet ? (
-      <p>This Chirp doesn't exist</p>
-    ) : (
-      <div className="tweet">{JSON.stringify(this.props.tweet)}</div>
+    const { tweet } = this.props;
+
+    if (!tweet) return <p>This Chirp doesn't exist</p>;
+
+    const {
+      name,
+      avatar,
+      timestamp,
+      text,
+      likes,
+      replies,
+      hasLiked,
+      parent
+    } = tweet;
+
+    return (
+      <div className="tweet">
+        <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+        <div className="tweet-info">
+          <div>
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button
+                className="replying-to"
+                onClick={(e) => this.toParent(e, parent.id)}
+              >
+                Replying to @ {parent.author}
+              </button>
+            )}
+            <p>{text}</p>
+          </div>
+          <div className="tweet-icons">
+            <BsFillReplyFill className="tweet-icon" />
+            <span>{replies !== 0 && replies}</span>
+            <button
+              className="heart-button"
+              onClick={(e) => this.handleHeart(e)}
+            >
+              {hasLiked ? (
+                <BsHeartFill size={20} color="blue" className="tweet-icon" />
+              ) : (
+                <BsHeart size={20} className="tweet-icon" />
+              )}
+            </button>
+            <span>{likes !== 0 && likes}</span>
+          </div>
+        </div>
+      </div>
     );
   }
 }
