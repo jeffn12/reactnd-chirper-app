f21fdd58b9343e9f247f430311e086dbc98a08f4
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-16 11:04:13 -0400

    feat: combine reducers into one export using redux

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import authedUser from "./authedUser";
+import tweets from "./tweets";
+import users from "./users";
+
+export default combineReducers({ authedUser, tweets, users });
