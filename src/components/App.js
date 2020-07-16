5a05670b9aa02a6b07ada16caddb9d6b3652344e
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-16 11:32:45 -0400

    feat: fetch initial data for store

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
 class App extends Component {
+  componentDidMount = () => {
+    this.props.dispatch(handleInitialData());
+  };
+
   render() {
     return <h1>Welcome to the Chirpy App, a Twitter clone.</h1>;
   }
 }
 
-export default App;
+export default connect()(App);
