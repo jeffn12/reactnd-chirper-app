2fedb4471ce65f58e7ae5d7a076d5d1ef3476e0b
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-16 11:11:20 -0400

    feat: create the store with redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+// Components
 import App from "./components/App";
+// Libraries
+import { createStore } from "redux";
+// State Assets
+import reducer from "./reducers";
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById("root"));
