54a4bbaac4836c539fd3cd40274217fe9d39a587
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-16 11:13:03 -0400

    feat: add store to context with react-redux

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import "./index.css";
 import App from "./components/App";
 // Libraries
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 // State Assets
 import reducer from "./reducers";
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
